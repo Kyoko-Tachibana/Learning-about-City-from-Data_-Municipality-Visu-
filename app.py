6de2c5affa47ae3eb291089dd407bd756d844a09
Kyoko-Tachibana <156287780+Kyoko-Tachibana@users.noreply.github.com>
2024-01-13 19:07:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.MORPH], suppress_callback_
         {'name': 'keywords', 'content': 'city, politics, data, graph, 街, 政治, データ, グラフ'},
         {'name': 'author', 'content': 'https://github.com/Kyoko-Tachibana'},
         {'name': 'twitter:card', 'content':'summary'},
-        {'name': 'twitter:image', 'content':'url(assets/sumnail.jpg)'}
+        {'name': 'twitter:image', 'content':'url(assets/sumnail.png)'}
     ])
 
 server = app.server
