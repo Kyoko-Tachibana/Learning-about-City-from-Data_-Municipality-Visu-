c94e97bdff9881876c84ee08a1ff0455dbd5ddb4
Ssk Ry <156287780+Kyoko-Tachibana@users.noreply.github.com>
2024-01-14 21:28:04 +0900

    Update city.py

--- pages/city.py
+++ pages/city.py
@@ -562,7 +562,7 @@ layout = dbc.Container([
                      'font': 'italic 1.2rem "Fira Sans", serif'},
         color='dark',
         sticky='bottom',
-        style={'height': 25},
+        style={'height': 25, 'width':'100vw'},
     ),
     style={'position': 'sticky', 'bottom': 0, 'height': '7vh'}
 )
