3abba7c1605d07620a63798e4c09fc7f978eee57
Ssk Ry <156287780+Kyoko-Tachibana@users.noreply.github.com>
2024-01-14 19:28:30 +0900

    Update index.py

--- pages/index.py
+++ pages/index.py
@@ -38,25 +38,27 @@ layout = html.Div([
         dbc.Col(dcc.Markdown('''
             **『まちラーン』**
         ''', 
-        style={'textAlign': 'center', 'font-family': '游明朝', 'font-size': 40, 'color': 'midnightblue'}
+        style={'textAlign': 'center', 'font-family': '游明朝', 'font-size': 40, 'color': 'midnightblue', 
+               'background-color':'lavender'}
         )),
-    ], style={'margin': 0, 'position': 'relative', 'top': '50%', 'transform': 'translateY(-50%)'}),
+    ], style={'margin': 0, 'position': 'relative', 'top': '40%', 'transform': 'translateY(-50%)'}),
 
     dbc.Row([
         dbc.Col(dcc.Markdown('''
             **まちラーンでは各ページに表示されている、西東京市のデータに基づいた分析結果を見るのはもちろんのこと、**
+            
             **自分で作成したデータセットをアップロードすることで、それらと同様の分析をほかの自治体に対して行うこともできます。**
         ''', 
         style={'textAlign': 'center', 'font-family': '游明朝', 'font-size': 15, 'color': 'black'}
         )),
-    ], style={'margin': 0, 'position': 'relative', 'top': '50%', 'transform': 'translateY(-50%)'}),
+    ], style={'margin': 0, 'position': 'relative', 'top': '40%', 'transform': 'translateY(-50%)'}),
 
     dbc.Row([
         dbc.Col(dbc.Button('政治', href='/politics', outline=True, 
                style={'font-family': 'Yu Gothic', 'font-size': 30, 'textAlign': 'center'}, color='light', size='lg')),
         dbc.Col(dbc.Button('都市', href='/city', outline=True,
                style={'font-family': 'Yu Gothic', 'font-size': 30, 'textAlign': 'center'}, color='light', size='lg')),
-    ], style={'margin': 0, 'position': 'relative', 'top': '50%', 'transform': 'translateY(-50%)'}),
+    ], style={'margin': 0, 'position': 'relative', 'top': '40%', 'transform': 'translateY(-50%)', 'left':'20%'}),
 
 ], style={'background-image': 'url(assets/city_img.jpg)', 'height': '100vh', 
           'background-size': 'cover', 'background-position': 'center', 'margin': 0})
