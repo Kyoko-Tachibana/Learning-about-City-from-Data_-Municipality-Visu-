4104b97ade9540c7ca47dca9f7b28836bc2676e8
Ssk Ry <156287780+Kyoko-Tachibana@users.noreply.github.com>
2024-01-17 01:27:49 +0900

    Update city.py

--- pages/city.py
+++ pages/city.py
@@ -267,7 +267,7 @@ layout = html.Div([
         html.Br(),
         html.Br(),
         html.Div(dbc.Tabs(
-        dbc.Tab(label='POPULATION', tab_id = 'pop', children=[html.Br(),
+        dbc.Tab(label='POPULATION', tab_id = 'pop', id = 'pop_', children=[html.Br(),
             dbc.Spinner(
                 dcc.Graph(
                 figure=population_general(df_pop)), color='dark'),
