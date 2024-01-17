812a5b0743b38200c8b015d2be4707a9dc4c45a7
Ssk Ry <156287780+Kyoko-Tachibana@users.noreply.github.com>
2024-01-17 18:42:59 +0900

    Update index.py

--- pages/index.py
+++ pages/index.py
@@ -7,32 +7,6 @@
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 
-
-#layout = html.Div([
- #   html.Div(dcc.Markdown('''
-  #  **『まちラーン』**
-   # ''', 
-    #style={'textAlign':'center', 'font-family':'游明朝', 'font-size':40, 'background-color':'lavender'
-     #      }), 
-      #     style={'color':'midnightblue', 'position':'relative', 'left':400, 'top':190, 'width': '400px', 'height':'100px',
-       #           }),
-    #html.Div(dcc.Markdown('''
-    #**まちラーンでは各ページに表示されている、西東京市のデータに基づいた分析結果を見るのはもちろんのこと、**
-                          
-    #**自分で作成したデータセットをアップロードすることで、それらと同様の分析をほかの自治体に対して行うこともできます。**
-    #''', 
-    #style={'textAlign':'center', 'font-family':'游明朝', 'font-size':15})
-     #      , style={'position':'relative', 'top':190, 'left':200, 'width':'810px', 'color':'black'}),
-    #dbc.Button('政治', href = '/politics', outline=True, 
-     #          style={'font-family': 'Yu Gothic', 'font-size':30, 'textAlign':'center', 'position':'relative'
-      #                , 'top':200, 'left':300}, color='light', size='lg'),
-    #dbc.Button('都市', href = '/city', outline=True,
-     #          style={'font-family': 'Yu Gothic', 'font-size':30, 'textAlign':'center',
-      #          'position':'relative', 'top':200, 'left':650}, color='light', size='lg')
-#], style={'background-image':'url(assets/city_img.jpg)', 'height':'100%', 
- #         'background-size': 'cover', 'background-position':'center', 'margin-bottom':0, 
-  #        'margin-right':0})
-
 layout = html.Div([
     dbc.Row([
         dbc.Col(dcc.Markdown('''
@@ -55,10 +29,12 @@ layout = html.Div([
 
     dbc.Row([
         dbc.Col(dbc.Button('政治', href='/politics', outline=True, 
-               style={'font-family': '游明朝', 'font-size': '3.5vw', 'textAlign': 'center'}, color='light', size='lg')),
+               style={'font-family': '游明朝', 'font-size': '3.5vw', 'textAlign': 'center', 'color':'navy'}, 
+                           color='light', size='lg')),
         dbc.Col(dbc.Button('都市', href='/city', outline=True,
-               style={'font-family': '游明朝', 'font-size': '3.5vw', 'textAlign': 'center'}, color='light', size='lg')),
-    ], style={'margin': 0, 'position': 'relative', 'top': '45%', 'transform': 'translateY(-50%)', 'left':'20vw'}),
+               style={'font-family': '游明朝', 'font-size': '3.5vw', 'textAlign': 'center', 'color':'navy'}, 
+                           color='light', size='lg')),
+    ], style={'margin': 0, 'position': 'relative', 'top': '50%', 'transform': 'translateY(-50%)', 'left':'20vw'}),
 
 ],style={'background-image': 'url(assets/city_img.jpg)', 'height': '100vh', 
           'background-size': 'cover', 'background-position': 'center', 'margin': 0, 'overflow-x': 'hidden'}, lang='ja')
