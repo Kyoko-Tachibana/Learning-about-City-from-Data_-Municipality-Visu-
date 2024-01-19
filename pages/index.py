a5b6f35e1581c12d4ecb6f4ffc30c9bb032bf325
Ssk Ry <156287780+Kyoko-Tachibana@users.noreply.github.com>
2024-01-19 21:50:50 +0900

    Update index.py

--- pages/index.py
+++ pages/index.py
@@ -10,7 +10,7 @@ import dash_bootstrap_components as dbc
 layout = html.Div([
     dbc.Row([
         dbc.Col(dcc.Markdown('''
-            **『まちラーン』**
+            **『自治体Visu』**
         ''', 
         style={'textAlign': 'center', 'font-family': '游明朝', 'font-size': '4.5vw', 'color': 'midnightblue', 
                'background-color':'lavender', 'width':'40vw'}
@@ -21,7 +21,7 @@ layout = html.Div([
         dbc.Col(dcc.Markdown('''
             **政治、都市の二分野で西東京市のデータを可視化するほか、アップロードされたデータを可視化します。**
             
-            **また、それらの可視化結果を各種プラットフォームに統合可能な形式でダウンロードできます。**
+            **また、それらの可視化結果は各種プラットフォームに統合可能な形式でダウンロードできます。**
             
             **ご自身の自治体を魅力的に外部に発信したい方向けのサービスです。**
         ''', 
@@ -33,10 +33,24 @@ layout = html.Div([
         dbc.Col(dbc.Button('政治', href='/politics', outline=True, 
                style={'font-family': '游明朝', 'font-size': '3.5vw', 'textAlign': 'center', 'color':'navy'}, 
                            color='light', size='lg')),
+        dbc.Col(dcc.Markdown('''
+        **ガイド**
+        ''', id='guide', style={'color':'deeppink', 'font-size':'12px'})),
         dbc.Col(dbc.Button('都市', href='/city', outline=True,
                style={'font-family': '游明朝', 'font-size': '3.5vw', 'textAlign': 'center', 'color':'navy'}, 
                            color='light', size='lg')),
     ], style={'margin': 0, 'position': 'relative', 'top': '50%', 'transform': 'translateY(-50%)', 'left':'20vw'}),
+    
+    dbc.Tooltip(dcc.Markdown('''
+    ①「都市」「政治」どちらかのボタンを押してスタート！
+
+    ②最初に表示されているのは西東京市のデータを可視化したグラフです。グラフは触って動かすことが出来ます。気になるグラフを探してみましょう。
+
+    ③気になるグラフを見つけたら、自分の自治体用にそのグラフを作ってみましょう！ 指定されているフォーマットに従ってデータを作成、アップロードします。
+    （西東京市の方は、表示されているグラフをそのままダウンロードできます。また、一部アップロード機能に対応していないグラフがあります。）
+
+    ④表示されたグラフは、様々な用途に活用できる形式でダウンロードできます。いろいろな使い方を探してみてください！
+    ''', style={'font-size':'12px'}), target='guide', placement='top')
 
 ],style={'background-image': 'url(assets/city_img.jpg)', 'height': '100vh', 
           'background-size': 'cover', 'background-position': 'center', 'margin': 0, 'overflow-x': 'hidden'}, lang='ja')
